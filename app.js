8ca6b686d80bafa6e56a7ba4294150a8d0b61577
ivan <ipomajulca@gmail.com>
2020-05-29 21:18:29 -0400

    adding map

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 //This app starts a server and listens on port 3000 for connections
-const express = require('express') //get express
-const app = express() //our app    
-const port = 3001 //port to use
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
+const express = require("express"); //get express
+const app = express(); //our app
+const port = 3001; //port to use
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
 
 //Endpoint to get list of stores
-app.get('/stores', (req, res) => { //req=request,res=response
-  getListOfCollection('diledb', 'stores') //call function to return list of stores
+app.get("/stores", (req, res) => {
+  //req=request,res=response
+  getListOfCollection("diledb", "stores") //call function to return list of stores
     .then((result) => res.status(200).json(result)) //response=200 is ok
     .catch((err) => res.status(500).send(err)); //response=500 there is an error
 });
 
 getListOfCollection = (dbName, collectionName) => {
   //get the url to connect to Mongodb
-  const getDbUrl = require('./getDbUrl');
+  const getDbUrl = require("./getDbUrl");
 
   return new Promise(function (resolve, reject) {
     var dbUrl = getDbUrl();
-    var MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect(dbUrl, {
-      useUnifiedTopology: true
-    }, (err, client) => {
-      if (err) return console.error(err)
-      var dbo = client.db(dbName);
-      dbo.collection(collectionName).find({}).toArray(function (err, result) {
-        console.log(err);
-        if (err) throw err;
-        resolve(result);
-        client.close();
-      });
-    });
-  })
-}
+    var MongoClient = require("mongodb").MongoClient;
+    MongoClient.connect(
+      dbUrl,
+      {
+        useUnifiedTopology: true,
+      },
+      (err, client) => {
+        if (err) return console.error(err);
+        var dbo = client.db(dbName);
+        dbo
+          .collection(collectionName)
+          .find({})
+          .toArray(function (err, result) {
+            console.log(err);
+            if (err) throw err;
+            resolve(result);
+            client.close();
+          });
+      }
+    );
+  });
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,62 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import mapboxgl from "mapbox-gl";
 
 class App extends Component {
-  state = { stores: []}
-  componentDidMount() {
-    fetch('/stores').then(res => res.json())
-    .then(stores => this.setState({stores}))
+  constructor(props) {
+    super(props);
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiaXBvbWFqdWxjYSIsImEiOiJja2FzdGpzODcwNHNtMzBwaHVjM2lrYmN6In0.g4bNxSg91fcNoGqh47zYuw";
+    this.state = {
+      stores: [],
+      lng: -74.1123,
+      lat: 40.4,
+      zoom: 11,
+      map: null,
+    };
   }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    fetch("/stores")
+      .then((res) => res.json())
+      .then((stores) => {
+        this.setState({
+          stores,
+        });
+        for (let item in stores) {
+          new mapboxgl.Marker()
+            .setLngLat([stores[item].lng, stores[item].lat])
+            .addTo(map);
+        }
+      });
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
+
   render() {
-    return(
+    return (
       <div>
-      <h1>Stores</h1>
-      <ul>{this.state.stores.map(store =>
-      <li key={store._id}>{store.name}</li>
-      )}</ul>
+        <h1> Stores </h1>
+        <ul>
+  
+          {this.state.stores.map((store) => (
+            <li key={store._id}> {store.name} </li>
+          ))}
+        </ul>
+        <div className="sidebarStyle"></div>
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
       </div>
     );
   }
