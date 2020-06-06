cb27582a6a952111fdd63e7c3436e719487eb144
ivan <ipomajulca@gmail.com>
2020-06-06 08:39:36 -0400

    adding mongodb url

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 // getting-started.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+const url =
+    "mongodb+srv://coin:coin123@cluster0-p7ucd.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(url, {useNewUrlParser: true});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
+  console.log("we are connected!");
 });
